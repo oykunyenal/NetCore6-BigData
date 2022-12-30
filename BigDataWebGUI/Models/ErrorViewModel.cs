463ddb3813aa064afdf690e9cb6499cbb8ca8906
OYENAL <OYENAL@DESKTOP-9PDS8TP>
2022-12-30 13:35:53 +0300

    Initial Commit

---
+++ BigDataWebGUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BigDataWebGUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
